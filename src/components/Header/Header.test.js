d83fc7710bca9824549eb86e3b89ab57279f4d84
vogelino <contact@vogelino.com>
2018-10-24 18:33:16 +0200

    #251 Add enzyme and an example test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { shallow } from 'enzyme';
 import { pages } from '../App';
 import Header from '.';
+import { HeaderLink } from './styles';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<Router><Header pages={pages} /></Router>, div);
 	ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should render the same amount of links as given to header', () => {
+	const header = shallow(<Header pages={pages} />);
+	const links = header.find(HeaderLink);
+	expect(links.length).toBe(pages.length);
 });
 
---
+++ src/tempPolyfills.js
@@ -0,0 +1,6 @@
+global.requestAnimationFrame = (cb) => {
+	setTimeout(cb, 0);
+};
+
+export default global.requestAnimationFrame;
+
