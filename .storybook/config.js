231608f92346eb656b6e17542eb2087db87b284a
vogelino <contact@vogelino.com>
2018-10-24 19:05:40 +0200

    #263 Add storybook's addon withInfo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,13 @@
 import { configure } from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
+
+setDefaults({
+	header: false,
+});
 
 function loadStories() {
-  require('../src/stories');
+	require('../src/stories'); // eslint-disable-line global-require
 }
 
 configure(loadStories, module);
+
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,12 +3,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-	.add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+	.add('with text', withInfo(`
+      description or documentation about my component, supports markdown
+    
+      ~~~js
+      <Button>Click Here</Button>
+      ~~~
+    
+    `)(() => <Button onClick={action('clicked')}>Hello Button</Button>))
 	.add('with some emoji', () => <Button onClick={action('clicked')}>Yippieee!</Button>);
 
