5db4f6eea842e511af6ee0fa94dd785da695570a
vogelino <contact@vogelino.com>
2018-11-01 21:43:28 +0100

    Add first documentation to README.md

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,7 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+import { setDefaults } from '@storybook/addon-info';
 
+setDefaults({
+	header: false,
+});
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,4 @@
 import { configure } from '@storybook/react';
-import { setDefaults } from '@storybook/addon-info';
-
-setDefaults({
-	header: false,
-});
 
 function loadStories() {
 	require('../src/stories'); // eslint-disable-line global-require
