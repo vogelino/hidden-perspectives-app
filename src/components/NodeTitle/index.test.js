963fe549cf238bbe40eeb311e08a1af8b64a2f23
vogelino <contact@vogelino.com>
2018-12-13 00:20:03 +0100

    #270 - Add NodeTitle Component

--- src/components/NodeTitle/index.test.js
+++ src/components/NodeTitle/index.test.js
@@ -3,7 +3,12 @@ import { shallow } from 'enzyme';
 import NodeTitle from './NodeTitle';
 
 describe('NodeTitle component', () => {
-	const fieldset = shallow(<NodeTitle additionalInfo="Yo what's up?">Hey!</NodeTitle>);
+	const fieldset = shallow(
+		<NodeTitle
+			title="Yo what's up?"
+			subtitle="Hey!"
+		/>,
+	);
 
 	it('should render without crashing', () => {
 		expect(fieldset.exists()).toBe(true);
--- src/pages/documentMetadata/index.test.js
+++ src/pages/documentMetadata/index.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Client from '../../state';
 import MetadataPage from '.';
 
@@ -9,7 +10,7 @@ const match = {
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<Client><MetadataPage match={match} /></Client>, div);
+	ReactDOM.render(<Router><Client><MetadataPage match={match} /></Client></Router>, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
 
