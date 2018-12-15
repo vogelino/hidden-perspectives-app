5eba42cd46df6279f4546ace93cb7ad9300f2bd0
vogelino <contact@vogelino.com>
2018-12-15 17:38:49 +0100

    #270 - Add transcript view

--- src/components/TranscriptView/TranscriptView.js
+++ src/components/TranscriptView/TranscriptView.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+	pipe,
+	split,
+	map,
+	join,
+} from 'ramda';
+import LoadingIndicator from '../LoadingIndicator';
+import { LoadingContainer } from '../LoadingIndicator/styles';
+import {
+	Container,
+	Content,
+	Transcript,
+	Line,
+	LineContent,
+} from './styles';
 
-const TranscriptView = ({ id }) => (
-	<div>{`Transcript for: ${id}`}</div>
+const createLineKey = (idx) => join('-', ['line', idx]);
+const wrapIntoSpan = (line, idx) => (
+	<Line key={createLineKey(idx)}>
+		<LineContent>{line}</LineContent>
+	</Line>
+);
+
+const formatTranscript = pipe(
+	split('\n'),
+	map(wrapIntoSpan),
+);
+
+const TranscriptView = ({ isLoading, transcript }) => (
+	<Container>
+		<LoadingContainer isLoading={isLoading}>
+			<LoadingIndicator />
+		</LoadingContainer>
+		{transcript && !isLoading && (
+			<Content>
+				<Transcript>{formatTranscript(transcript)}</Transcript>
+			</Content>
+		)}
+	</Container>
 );
 
 TranscriptView.propTypes = {
-	id: PropTypes.string.isRequired,
+	transcript: PropTypes.string,
+	isLoading: PropTypes.bool,
+};
+
+TranscriptView.defaultProps = {
+	transcript: '',
+	isLoading: true,
 };
 
 export default TranscriptView;
--- src/components/TranscriptView/index.js
+++ src/components/TranscriptView/index.js
@@ -1,3 +1,39 @@
+import { compose, withState, lifecycle } from 'recompose';
+import { withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withLoading, withErrors, getErrorHandler } from '../../utils/hocUtil';
 import TranscriptView from './TranscriptView';
 
-export default TranscriptView;
+const TRANSCRIPT_QUERY = gql`
+	query GetDocument($id: ID!) {
+		Document(id: $id) {
+			id
+			documentTranscript
+		}
+	}
+`;
+
+
+const getDataParser = ({ setTranscript, stopLoading }) => ({ data }) => {
+	setTranscript(data.Document.documentTranscript);
+	stopLoading();
+};
+
+export default compose(
+	withApollo,
+	withErrors,
+	withLoading,
+	withState('transcript', 'setTranscript', ''),
+	lifecycle({
+		componentDidMount() {
+			const { id, client } = this.props;
+
+			client.query({
+				query: TRANSCRIPT_QUERY,
+				variables: { id },
+			})
+				.then(getDataParser(this.props))
+				.catch(getErrorHandler(this.props));
+		},
+	}),
+)(TranscriptView);
