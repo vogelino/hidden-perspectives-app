f3fd440e6211ef3ec128b8bb845774dd237e81dd
vogelino <contact@vogelino.com>
2019-02-01 10:48:41 +0100

    #54 - Add placeholder for LabelFilters

---
+++ src/components/LabelFilters/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	flex: 0 0 28rem;
+	height: calc(100vh - 4.5rem);
+	background: white;
+	position: relative;
+	z-index: 1;
+`;
--- src/components/SummarySection/styles.js
+++ src/components/SummarySection/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-	flex: 0 0 32rem;
+	flex: 0 0 28rem;
 	height: calc(100vh - 4.5rem);
 	background: white;
 	position: relative;
