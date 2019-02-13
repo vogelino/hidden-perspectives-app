60ece1dc61db6252789e991d092ac1c4d93a3ae3
Lucas Vogel <contact@vogelino.com>
2019-02-13 14:15:28 +0100

    #96 - Hide signup in production

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,19 +9,24 @@ import {
 	Logo,
 } from './styles';
 
+// REMOVE THE PRODUCTION CHECK WHEN YOU PLAN TO INTRODUCE SIGN UP
+const isProduction = process && process.env && process.env.NODE_ENV === 'production';
+
 const Header = ({
 	isAuthenticated,
 }) => (
 	<HeaderContainer>
 		<Logo to="/">Hidden Perspectives</Logo>
 		<Search />
-		{!isAuthenticated() ? (
-			<UserInfoContainer>
-				<LogButton to="/login" exact>
-					{'Login'}
-				</LogButton>
-			</UserInfoContainer>
-		) : <UserInfo />}
+		{!isProduction && (
+			!isAuthenticated() ? (
+				<UserInfoContainer>
+					<LogButton to="/login" exact>
+						{'Login'}
+					</LogButton>
+				</UserInfoContainer>
+			) : <UserInfo />
+		)}
 	</HeaderContainer>
 );
 
